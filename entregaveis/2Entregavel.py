a3ef2c6b7f60bec873c1c0320fae38aa5f41b82b
DorivalRamos <dorival.millan@bandtec.com.br>
2021-06-21 19:36:26 -0300

    Apenas deixando o codigo comentado

--- entregaveis/2Entregavel.py
+++ entregaveis/2Entregavel.py
@@ -1,7 +1,17 @@
+# Projeto 02 – Jogo Jokenpô
+# Utilizando os conceitos aprendidos até estruturas de repetição, crie um programa que jogue pedra, papel e tesoura (Jokenpô) com você. O programa tem que:
+
+# • Permitir que eu decida quantas rodadas iremos fazer;
+# • Ler a minha escolha (Pedra, papel ou tesoura);
+# • Decidir de forma aleatória a decisão do computador;
+# • Mostrar quantas rodadas cada jogador ganhou;
+# • Determinar quem foi o grande campeão de acordo com a quantidade de vitórias de cada um (computador e jogador);
+# • Perguntar se o Jogador quer jogar novamente, se sim inicie volte a escolha de quantidade de rodadas, se não finalize o programa.
+
 from random import randint
 from time import sleep
 import os
-
+# Variaveis que serão usadas para compor o código
 itens = ("Papel", "Pedra", "Tesoura")
 computador = randint(0, 2)
 tentativas = int()
@@ -20,7 +30,9 @@ for c in range(pergunta):
     print(f"Você escolheu {itens[jogador]}")
     sleep(1)
     print("-=" * 12)
-    if computador == 0:
+    # Aqui começa os If e Else, recebe o valor do Computador e compara com o valor Jogador(itens)
+
+    if computador == 0:  # Se computador escolheu Papel
         if jogador == 0:  # Jogador escolheu Papel
             print("Empate")
         elif jogador == 1:  # Jogador escolheu Pedra
@@ -29,7 +41,7 @@ for c in range(pergunta):
             print("Você ganhou !!!")
             tentativas += 1
 
-    if computador == 1:
+    if computador == 1:  # Se computador escolheu Pedra
         if jogador == 0:  # Jogador escolheu Papel
             print("Você ganhou")
             tentativas += 1
@@ -38,7 +50,7 @@ for c in range(pergunta):
         elif jogador == 2:  # Jogador escolheu Tesoura
             print("Você perdeu !!!")
 
-    if computador == 2:
+    if computador == 2:  # Se computador escolheu Tesoura
         if jogador == 0:  # Jogador escolheu Papel
             print("Você Perdeu")
         elif jogador == 1:  # Jogador escolheu Pedra
@@ -46,6 +58,7 @@ for c in range(pergunta):
             tentativas += 1
         elif jogador == 2:  # Jogador escolheu Tesoura
             print("Você Empatou !!!")
-    os.system("cls") or None
+
+    os.system("cls") or None  # O comando para limpar o terminal a cada lupping
 
 print(f"Você ganhou {tentativas} Vezes!!")
