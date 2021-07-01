9b3f410ab7dcae519f4eac75765a928c399fa9a1
DorivalRamos <dorival.millan@bandtec.com.br>
2021-07-01 19:07:28 -0300

    retirando os projetos desse repositorio

--- Aulas/Aula30_06/conta.py
+++ Aulas/Aula30_06/conta.py
@@ -1,34 +1,34 @@
 class Conta:
     def __init__(self, titular, saldo=10):
         self.titular = titular
-        self.saldo = saldo
+        self.__saldo = saldo
 
     def __str__(self):
-        if self.saldo == 0:
+        if self.__saldo == 0:
             return f'''
                   Titular: {self.titular}
                   '''
         else:
             return f'''
                   Titular: {self.titular}
-                  Saldo: {self.saldo}
+                  Saldo: {self.__saldo}
                   '''
 
     def depositar(self, valor):
-        self.saldo += valor
-        return f'{self.titular}seu saldo atual é {self.saldo}'
+        self.__saldo += valor
+        return f'{self.titular}seu saldo atual é {self.__saldo}'
 
     def sacar(self, valor):
         if valor > 0:
-            if self.saldo <= 0 or self.saldo < valor:
+            if self.__saldo <= 0 or self.__saldo < valor:
                 print('Saldo insuficiente para realizar a ação!!!')
                 self.extrato()
             else:
-                self.saldo -= valor
+                self.__saldo -= valor
                 print(f"voçê sacou R${valor:.2f} com sucesso!!")
                 self.extrato()
         else:
             print('Você não pode sacar ZERO reais')
 
     def extrato(self):
-        return f'Saldo: R${self.saldo}'
+        return f'Saldo: R${self.__saldo}'
